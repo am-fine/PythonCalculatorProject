69dfa0d06c5aabe95002e976b55e2d9bfbc0f12b
jd <jdpatio14@gmail.com>
2025-12-02 19:14:21 -0500

    finished the calc!!! auto text resizing added.

--- Simple Calculator [ADVANCED+].py
+++ Simple Calculator [ADVANCED+].py
@@ -7,7 +7,7 @@
 import sys
 from pathlib import Path
 from PySide6.QtWidgets import *
-from PySide6.QtGui import QColor
+from PySide6.QtGui import QColor, QFont, QFontMetrics
 from pynput import keyboard
 import darkdetect
 # =============== #
@@ -26,29 +26,38 @@ def get_stylesheet(app):
     theme = None
 
     if darkdetect.isLight():
-        theme = script_dir / light_qss
         # if darkdetect detects the user's system theme as light, the
         # code will read the light style sheet, likewise for if the
         # system theme is dark.
+        theme = script_dir / light_qss
+        # the "/" symbol is what joins them as one directory.
     elif darkdetect.isDark():
+        
         theme = script_dir / dark_qss
+        
     else:
+        
         print("no theme found!")
     
     if theme:
         # means if a theme is found (if theme = None this will not run.)
         try:
+            
             with open(theme, "r") as file:
+                
                 app.setStyleSheet(file.read())
                 # opens the style sheet and reads it, sets it as the
                 # application's style sheet.
                 print(f"loaded stylesheet from: {theme}")
             
         except FileNotFoundError:
+            
             print(f"file not found at {theme}")
             # if no sheet is found, it will print a message, likewise
             # if another error occurs.
+            
         except Exception as error:
+            
             print(f"unexpected error occurred: {error}")
 # =========================== #
 
@@ -69,8 +78,11 @@ class CalculatorApp:
         application.
         """
         self._currently_typed = ""
+        # this will hold the user's current input.
         self._full_expression = ""
-        self._can_input_operator = True
+        # this will hold the assembled expression.
+        self._can_input_operator = False
+        # set to false to prevent the first value from being an operator
         self.button_effects = []
         # a container is needed to hold all of the drop shadows that the
         # buttons have.
@@ -105,20 +117,24 @@ class CalculatorApp:
         self._start_keyboard_listener()
         self.create_buttons()
         # when the class is initialized (which will be at the end), 
-        # these 3 things will happen.
+        # these 3 things will happen, it looks like they will happen
+        # before the functions are actually defined, but they won't.
     
     def _setup_ui(self):
         """
         Makes the layout and main UI of the application.
         """
-        # - setting up the main window - #
+        # === MAIN WINDOW SETUP === #
         self.window = QMainWindow()
         self.window.resize(320,380)
+        # this sets the starting size of the window
         self.window.setObjectName("mainWindow")
         # this is so the window can be styled, this will occur with the
         # other elements as well like the buttons, text, etc.
         self.window.show()
-        # - setting up the layout of buttons, text, etc. - #
+        # ========================= #
+        
+        # === LAYOUT SETUP === #
         self.central = QWidget()
         self.window.setCentralWidget(self.central)
         # this sets self.central as the container for all the content of
@@ -127,7 +143,9 @@ class CalculatorApp:
         # we then make it so it arranges the content in a grid.
         self.layout.setSpacing(10)
         self.layout.setContentsMargins(10, 10, 10, 10)
-        # - setting up the display window - #
+        # ==================== #
+        
+        # === DISPLAY WINDOW SETUP === #
         self.display_window = QLabel()
         # display_window is what will hold the user's inputs.
         self.display_window.setObjectName("displayWindow")
@@ -139,7 +157,9 @@ class CalculatorApp:
         # elements as well, just like setting their object name.
         self.display_window.setContentsMargins(0, 0, 0, 10)
         self.layout.addWidget(self.display_window, 1, 0, 1, 5)
-        # - setting up the calculate window - #
+        # ============================ #
+        
+        # === CALCULATE WINDOW SETUP === #
         self.calculate_window = QLabel()
         # the calculate window is what will hold the equation the user
         # has made so far.
@@ -149,6 +169,9 @@ class CalculatorApp:
         # another instance of allowing a widget to automatically resize.
         self.calculate_window.setContentsMargins(1, 0, 0, 80)
         self.layout.addWidget(self.calculate_window, 1, 0, 1, 5) 
+        # the calculate window technically overlaps with the display
+        # window, but because of the margins, the text will never touch
+        # ============================== #
         
     def _start_keyboard_listener(self):
         """
@@ -177,6 +200,56 @@ class CalculatorApp:
         self.button_shadow.setColor(QColor(0, 0, 0, 15))
         return self.button_shadow
     
+    def _resize_font(self, label: QLabel, text: str, max_font_size):
+        """
+        Dynamically resizes text in the calculator to make sure it fits
+        in the window.
+        
+        Args:
+            label (QLabel): the label which the text is located in,
+                            e.g. self._display_window
+            text (str): the text itself
+            max_font_size (int): the maximum set size for the text
+        """
+        
+        if not text:
+            font = QFont(label.font())
+            font.setPointSize(max_font_size)
+            label.setFont(font)
+            return
+        # if there is no text, the font size will automatically be set
+        # to the largest it can be.
+        
+        font = QFont(label.font())
+        # makes a copy of the font from the label.
+        label_width = label.width()
+        # gets the width of the label.
+        current_font_size = max_font_size
+        font.setPointSize(current_font_size)
+        # applies the default size (maximum) to the text.
+        
+        while current_font_size > 12:
+            # as long as the font is above this threshold (this int
+            # would be the minimum size.)
+            
+            metrics = QFontMetrics(font)
+            # QFontMetrics allows us to measure the amount of space the
+            # text would take up.
+            text_width = metrics.horizontalAdvance(text)
+            # this is what measures that.
+            if text_width <= label_width:
+                # once the text width fits inside the label width the 
+                # loop will stop resizing the font.
+                break
+        
+            current_font_size -= 1
+            font.setPointSize(current_font_size)
+            # resizes and applies the font
+
+        label.setFont(font)
+        # applies the changes to the label, if there is no change then
+        # it will just be set to max_font_size.
+        
     def _is_operator(self, value):
         """
         Checks if the value given is an operator in the list.
@@ -222,12 +295,17 @@ class CalculatorApp:
         final_expression = self._full_expression + self._currently_typed
             
         try:
-            answer = str(eval(final_expression))
+            answer = float(eval(final_expression))
+            # answer as a float for scientific notation
             # eval() is not a very safe method to use unless the input
             # is filtered like a calculator.
             # this runs the expression as python code, which calculates
             # the answer.
+            answer = str(answer)
+            # convert it to a string so we can set text.
             self.calculate_window.setText(final_expression + " =")
+            # add an equals sign at the end to mark the end of the
+            # expression.
             self.display_window.setText(answer)
                 
             self._currently_typed = answer
@@ -235,6 +313,8 @@ class CalculatorApp:
             # equations off from it.
             self._full_expression = ""
             self._can_input_operator = True
+            # this is true so that the user can evaluate from their
+            # answer.
             
         except Exception as error:
             # handles any error
@@ -263,14 +343,50 @@ class CalculatorApp:
             return
             
         if self._can_input_operator:
-
-            self._full_expression += self._currently_typed + operator
-            # gather what has currently been typed + the operator.
-            self.calculate_window.setText(self._full_expression)
             
-            self._currently_typed = ""
-            self.display_window.setText("")
+            can_eval = any(operator in self._full_expression for operator in 
+                          ["+", "-", "*", "/"])
+            # flags if the expression has an operator already.
+            self._full_expression += self._currently_typed
+            # we add what has been currently typed to the full
+            # expression.
+            if can_eval:
+                # if can_eval is True then this will calculate whatever
+                # is in calculate_window to shorten the expression.
+                try:
+                    
+                    partial_answer = float((eval(self._full_expression)))
+                    # calculate what the user has in the calculate
+                    # window (as a float so that scientific notation
+                    # will be used if the number is too big or small.)
+                    self._full_expression = str(partial_answer)
+                    # convert it to a string so we can set text.
+                    self._full_expression += operator
+                    # add back the operator so the user knows what they
+                    # are calculating.
+                    self.calculate_window.setText(self._full_expression)
+                    self.display_window.setText("")
+                    
+                except ZeroDivisionError:
+                    # only error that could happen here is a division by
+                    # zero, so if that happens we just tell the user
+                    # it's undefined.
+                    self.display_window.setText("Undefined")
+                    
+                    self.calculate_window.setText("")
+                    self._currently_typed = ""
+                    self._full_expression = ""
                 
+            else:
+                # happens only when they type their first operator.
+                self._full_expression += operator
+                # gather what has currently been typed + the operator.
+                self.calculate_window.setText(self._full_expression)
+                
+                self.display_window.setText("")
+                 
+            self._currently_typed = ""
+            
             self._can_input_operator = False
             
         else:
@@ -286,7 +402,7 @@ class CalculatorApp:
             # if there is already a decimal, it won't allow another.
             return
 
-        if len(self._currently_typed) < 12:
+        if len(self._currently_typed) < 16:
             # limits the input amount to 12 characters, if the amount in
             # self._currently_typed is above 12, this won't run.
             self._currently_typed += value
@@ -305,19 +421,34 @@ class CalculatorApp:
             value (str): the value of a button (e.g "5", "*", or "C")
         """
         if value == "=":
+            
             self._calculate()
+            self._resize_font(self.display_window, self._currently_typed, 47)
+            self._resize_font(self.calculate_window,
+                              self.calculate_window.text(), 17)
             
         elif value == "CE":
+            
             self._clear_entry()
+            self._resize_font(self.display_window, self._currently_typed, 47)
             
         elif value == "C":
+            
             self._clear_all()
+            self._resize_font(self.display_window, self._currently_typed, 47)
+            self._resize_font(self.calculate_window,
+                              self.calculate_window.text(), 17)
             
         elif self._is_operator(value):
+            
             self._handle_operator(value)
+            self._resize_font(self.calculate_window, 
+                              self.calculate_window.text(), 17)
             
         elif value.isdigit() or value == ".":
+            
             self._handle_number(value)
+            self._resize_font(self.display_window, self._currently_typed, 47)
             
     def key_press(self, key):
         """Handles keyboard inputs."""
